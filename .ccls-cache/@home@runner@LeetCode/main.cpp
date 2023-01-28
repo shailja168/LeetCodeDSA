95c84357c85b3d7c5fc374890fd9cbfa7b2c7442
Shailja Tripathi <75543416+shailja168@users.noreply.github.com>
2023-01-28 09:23:06 +0000

    LeetCode DSA sheet by Fraz

--- .ccls-cache/@home@runner@LeetCode/main.cpp
+++ .ccls-cache/@home@runner@LeetCode/main.cpp
@@ -1,5 +1,25 @@
-#include <iostream>
+// two pointer approach
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int,int>> v;
+
+        for(int i =0; i<n;i++)v.push_back({nums[i],i});
+        sort(v.begin(),v.end());
+        int x=0,y=n-1;
+        while(x<y){
+            int sum = v[x].first + v[y].first;
+            if(sum == target)return {v[x].second,v[y].second};
+            else if(sum>target) y--;
+            else x++;
+        }
+        return {-1,-1};
+    }
+};
+
