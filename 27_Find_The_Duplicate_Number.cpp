ecd5ef2d88abc743a261ac1ba532001478ce2eda
Shailja Tripathi <75543416+shailja168@users.noreply.github.com>
2023-03-10 21:22:09 +0000

    Q27

---
+++ 27_Find_The_Duplicate_Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> freq(nums.size(),0);
+        for(int i = 0;i<nums.size();i++) freq[nums[i]]++;
+
+        for(int i = 0;i<freq.size();i++){
+            if(freq[i]  >= 2 ){
+                return i;
+            }
+        }
+        return 1;
+    }
+};
